3f0faafb822f770ef28f1195aa983ac0bbfad6a1
Dennis Chung <chungdennis6@gmail.com>
2019-04-08 00:08:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ end
   
   def apply_discount(title, price, discount)
     if @discount > 0
-    "After the discount, the total comes to $#{@total -= @discount * 30}."
+    "After the discount, the total comes to $#{ @total -= @discount * 10}."
   else
     "There is no discount to apply"
   end
